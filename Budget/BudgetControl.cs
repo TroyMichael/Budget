aa21e8b1d905761c286bb0b972b7972ee9f97f4c
Michael Troy <troy.michael@hotmail.com>
2016-06-01 18:43:58 +0200

    Add project files.

---
+++ Budget/BudgetControl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget
+{
+    public class BudgetControl
+    {
+        private LinkedList<Expense> _expenses;
+        public LinkedList<Expense> expense
+        {
+            get { return _expenses; }
+        }
+
+        public BudgetControl()
+        {
+            _expenses = new LinkedList<Expense>();
+        }
+
+        public void AddExpense(string name, double value)
+        {
+            if (!string.IsNullOrEmpty(name) && value <= 0)
+            {
+                _expenses.AddLast(new Expense(name, value));
+            }
+        }
+    }
+}
---
+++ BudgetTests/BudgetControlTests.cs
@@ -0,0 +1,26 @@
+﻿using Budget;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetTests
+{
+    [TestFixture]
+    class BudgetControlTests
+    {
+
+        [Test]
+        public void AddExpense_ValidExpense_ExpenseCountIs1()
+        {
+            BudgetControl budget = new BudgetControl();
+
+            budget.AddExpense("Katzenfutter", -10);
+
+            Assert.AreEqual(1, budget.expense.Count);
+        }
+
+    }
+}
